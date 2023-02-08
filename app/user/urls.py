86db99b196e21be975c6964777de31427f62e5d7
Sujantamboli <sujanattar2000@gmail.com>
2023-02-08 18:24:39 +0530

    user token authentication

--- app/user/urls.py
+++ app/user/urls.py
@@ -7,5 +7,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("create/", views.CreateUserView.as_view(), name="create")
+    path("create/", views.CreateUserView.as_view(), name="create"),
+    path("token/", views.CreateTokenView.as_view(), name="token"),
 ]
---
+++ start_server.sh
@@ -0,0 +1,8 @@
+source venv/Scripts/activate
+
+set MYSQL_DATABASE=starwarsdb
+set MYSQL_USER=root
+set MYSQL_ROOT_PASSWORD=root
+set MYSQL_HOST=127.0.0.1
+
+python manage.py runserver
