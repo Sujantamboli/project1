a83c3d6bda757789335a2f957816af155bbd8712
Sujantamboli <sujanattar2000@gmail.com>
2023-02-10 18:20:46 +0530

    test user profile

--- start_server.sh
+++ start_server.sh
@@ -1,8 +1,8 @@
 source venv/Scripts/activate
 
-set MYSQL_DATABASE=starwarsdb
-set MYSQL_USER=root
-set MYSQL_ROOT_PASSWORD=root
-set MYSQL_HOST=127.0.0.1
+export MYSQL_DATABASE=starwarsdb
+export MYSQL_USER=root
+export MYSQL_ROOT_PASSWORD=root
+export MYSQL_HOST=127.0.0.1
 
-python manage.py runserver
+python app/manage.py runserver
